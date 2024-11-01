d5daa630bba13b31204c0487e511d1710f14e77b
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2024-11-01 13:37:08 -0700

    help command

---
+++ commands/Help/Help.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Provides a list of all available commands or detailed info on a specific command')
+    .addStringOption(option =>
+      option
+        .setName('command')
+        .setDescription('The specific command you want more information about')
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const commandName = interaction.options.getString('command')
+    const commands = {
+      daily: 'Claim your daily reward. Available once per day.',
+      hunt: 'Hunt creatures and earn rewards based on your encounters.',
+      list: 'ADMIN ONLY: Lists the number of monsters by CR tier or search by monster name.',
+      shop: 'Buy monster packs to expand your collection.',
+      free: 'Claim a free reward every 8 hours.'
+    }
+
+    if (commandName) {
+      // Detailed information for a specific command
+      const commandInfo = commands[commandName.toLowerCase()]
+      if (!commandInfo) {
+        await interaction.reply({
+          content: `Command "${commandName}" not found.`,
+          ephemeral: true,
+        })
+        return
+      }
+      
+      const commandEmbed = new EmbedBuilder()
+        .setColor('#00FF00')
+        .setTitle(`Help: /${commandName}`)
+        .setDescription(commandInfo)
+      await interaction.reply({ embeds: [commandEmbed] })
+      
+    } else {
+      // General list of commands
+      const commandList = Object.keys(commands)
+        .map(cmd => `/${cmd}`)
+        .join(' ')
+
+      const embed = new EmbedBuilder()
+        .setColor('#00FF00')
+        .setTitle('Command List')
+        .setDescription('Use `/help <command name>` for more information')
+        .addFields(
+          { name: ':SSTier: Card Game', value: commandList }
+        )
+      await interaction.reply({ embeds: [embed] })
+    }
+  },
+}
