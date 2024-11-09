1e3f247594bcdad3c625694c4bee81d5f46bbed9
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2024-11-09 12:09:25 -0800

    energy refill

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ console.log(`Environment: ${process.env.NODE_ENV}`)
 const fs = require('node:fs')
 const path = require('node:path')
 const sequelize = require('./config/sequelize.js')
+const cron = require('node-cron')
 
 const { Client, Collection, GatewayIntentBits } = require('discord.js')
 const { User } = require('./Models/model')
@@ -90,6 +91,46 @@ const messageHandler = require('./handlers/messageHandler')
 
 // Call the function to set up message handling
 messageHandler(client, User)
+
+// Schedule a cron job to refill energy every 10 minutes
+cron.schedule('*/10 * * * *', async () => {
+  try {
+    const users = await User.findAll()
+
+    for (const user of users) {
+      let currency = user.currency || {
+        energy: 10,
+        gems: 0,
+        eggs: 0,
+        ichor: 0,
+        dice: 0,
+      }
+
+      if (typeof currency !== 'object') {
+        currency = { energy: 10, gems: 0, eggs: 0, ichor: 0, dice: 0 }
+      }
+
+      const currentEnergy = currency.energy || 0
+
+      if (currentEnergy < 10) {
+        currency.energy = Math.min(currentEnergy + 1, 10)
+        user.currency = currency
+        user.changed('currency', true)
+        await user.save()
+        console.log(
+          `User ID: ${user.user_id}, Updated Currency:`,
+          user.currency
+        )
+      } else {
+        console.log(`User ID: ${user.user_id} already has maximum energy.`)
+      }
+    }
+
+    console.log('Energy refilled for all users.')
+  } catch (error) {
+    console.error('Error refilling energy:', error)
+  }
+})
 
 // Log in to Discord with client's token
 client.login(process.env.TOKEN)
