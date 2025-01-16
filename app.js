11bb2899795b38f189a3cab2a4abd9929a03dbc1
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2025-01-16 12:39:57 -0800

    prevent energy refill in develop

--- app.js
+++ app.js
@@ -78,41 +78,43 @@ const messageHandler = require('./handlers/messageHandler')
 messageHandler(client, User)
 
 // Schedule a cron job to refill energy every 10 minutes
-cron.schedule('*/10 * * * *', async () => {
-  try {
-    const users = await User.findAll()
+if (process.env.NODE_ENV === 'production') {
+  cron.schedule('*/10 * * * *', async () => {
+    try {
+      const users = await User.findAll()
 
-    for (const user of users) {
-      let currency = user.currency || {
-        energy: 15,
-        gems: 0,
-        eggs: 0,
-        ichor: 0,
-        dice: 0,
-      }
-      if (typeof currency !== 'object') {
-        currency = { energy: 15, gems: 0, eggs: 0, ichor: 0, dice: 0 }
-      }
+      for (const user of users) {
+        let currency = user.currency || {
+          energy: 15,
+          gems: 0,
+          eggs: 0,
+          ichor: 0,
+          dice: 0,
+        }
+        if (typeof currency !== 'object') {
+          currency = { energy: 15, gems: 0, eggs: 0, ichor: 0, dice: 0 }
+        }
 
-      const currentEnergy = currency.energy || 0
-      if (currentEnergy < 15) {
-        currency.energy = Math.min(currentEnergy + 1, 15)
-        user.currency = currency
-        user.changed('currency', true)
-        await user.save()
-        console.log(
-          `User ID: ${user.user_id}, Updated Currency:`,
-          user.currency
-        )
-      } else {
-        console.log(`User ID: ${user.user_id} already has maximum energy.`)
+        const currentEnergy = currency.energy || 0
+        if (currentEnergy < 15) {
+          currency.energy = Math.min(currentEnergy + 1, 15)
+          user.currency = currency
+          user.changed('currency', true)
+          await user.save()
+          console.log(
+            `User ID: ${user.user_id}, Updated Currency:`,
+            user.currency
+          )
+        } else {
+          console.log(`User ID: ${user.user_id} already has maximum energy.`)
+        }
       }
+      console.log('Energy refilled for all users.')
+    } catch (error) {
+      console.error('Error refilling energy:', error)
     }
-    console.log('Energy refilled for all users.')
-  } catch (error) {
-    console.error('Error refilling energy:', error)
-  }
-})
+  })
+}
 
 // Log in to Discord with client's token
 client.login(process.env.TOKEN)
